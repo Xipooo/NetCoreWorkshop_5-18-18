4a2bb99e43b0b0fcc0c33e2a1fdaf904e977ed5f
Steve Bishop <xipooo@gmail.com>
2018-05-16 09:59:48 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreWorkshop_5_18_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
